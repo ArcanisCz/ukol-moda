a2574682d11db8b6188f5607bd33cd3547571f15
Jirinka <jirina.poustecka@greenfoxacademy.com>
2022-03-20 22:29:09 +0100

    add Header

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
+import Header from './components/Header';
 
 const App = () => (
-  <>
-    <p>Tady asi něco bude</p>
-  </>
+
+  <div className="container">
+    <Header title="Móda"></Header>
+
+  </div>
 );
 
 render(<App />, document.querySelector('#app'));
