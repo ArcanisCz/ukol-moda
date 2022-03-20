345a212e946f5ee68606f60c10482d4f31e0dd7b
Jirinka <jirina.poustecka@greenfoxacademy.com>
2022-03-20 22:43:26 +0100

    add Footer

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => (
 
   <div className="container">
     <Header title="Móda"></Header>
+    <Footer year={2022} author="Jirinka"></Footer>
 
   </div>
 );
