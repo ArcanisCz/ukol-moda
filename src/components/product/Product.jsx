00b65ea85db33ba24908c569a30e7fff2ceed8fd
Evita-M <eva.matova@seznam.cz>
2022-03-26 18:22:12 +0100

    Fix path of images

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -4,7 +4,7 @@ import "./style.css";
 const Product = ({ name, desc, price, imgSrc, imgAlt }) => {
   return (
     <div className="product">
-      <img className="product__image" src={`./assets/${imgSrc}`} alt={imgAlt} />
+      <img className="product__image" src={imgSrc} alt={imgAlt} />
       <div className="product__content">
         <h3 className="product__name">{name}</h3>
         <p className="product__description">{desc}</p>
