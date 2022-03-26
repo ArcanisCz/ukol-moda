c1511c28ff1c2b9cf3fae17c2a713ace93eb1566
Evita-M <eva.matova@seznam.cz>
2022-03-26 18:24:35 +0100

    Organise classes in product section component

--- src/components/products-section/ProductsSection.jsx
+++ src/components/products-section/ProductsSection.jsx
@@ -3,27 +3,30 @@ import Product from "../product/Product";
 import "./style.css";
 
 const ProductsSection = ({ title, products, category }) => {
+  const categoryClasses = {
+    dresses: `products-section--dresses`,
+    shoes: `products-section--shoes`,
+    accessories: `products-section--accessories`,
+  };
+
   return (
-    <>
-      <section className={`products-section products-section--${category}`}>
-        <h2>{title}</h2>
-        <div className="products">
-          {products.map((product, index) => {
-            const { name, desc, price, imgAlt, imgSrc } = product;
-            return (
-              <Product
-                key={index}
-                name={name}
-                desc={desc}
-                price={price}
-                imgAlt={imgAlt}
-                imgSrc={`/assets/${imgSrc}`}
-              />
-            );
-          })}
-        </div>
-      </section>
-    </>
+    <section className={`products-section ${categoryClasses[category]}`}>
+      <h2>{title}</h2>
+      <div className="products">
+        {products.map(({ name, desc, price, imgAlt, imgSrc, id }) => {
+          return (
+            <Product
+              key={id}
+              name={name}
+              desc={desc}
+              price={price}
+              imgAlt={imgAlt}
+              imgSrc={imgSrc}
+            />
+          );
+        })}
+      </div>
+    </section>
   );
 };
 
