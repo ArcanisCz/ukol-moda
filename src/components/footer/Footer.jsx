c60e118223ffc37faf317eff69d739ab9840beec
Evita-M <eva.matova@seznam.cz>
2022-03-26 18:21:35 +0100

    Remove unnecessary React fragment

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,17 +3,15 @@ import "./style.css";
 
 const Footer = ({ year, author }) => {
   return (
-    <>
-      <footer className="footer">
-        <p className="footer__copyright">
-          &copy; {year}, {author}
-        </p>
-        <p className="footer__disclosure">
-          Jsme fiktivní firma, která nic neprodává a v žádném případě ani za nic
-          neručí.
-        </p>
-      </footer>
-    </>
+    <footer className="footer">
+      <p className="footer__copyright">
+        &copy; {year}, {author}
+      </p>
+      <p className="footer__disclosure">
+        Jsme fiktivní firma, která nic neprodává a v žádném případě ani za nic
+        neručí.
+      </p>
+    </footer>
   );
 };
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,12 +3,10 @@ import "./style.css";
 
 const Header = ({ title, desc }) => {
   return (
-    <>
-      <header className="header">
-        <h1 className="header__title">{title}</h1>
-        <p className="header__description">{desc}</p>
-      </header>
-    </>
+    <header className="header">
+      <h1 className="header__title">{title}</h1>
+      <p className="header__description">{desc}</p>
+    </header>
   );
 };
 
